ce08bee3c85d4fe2c9b0c3e251c20b0bc09b7607
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-03-29 02:53:23 +0600

    initial commit: add password generator extension with basic functionality and configuration files

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Password Generator Changelog
+
+## [Initial Version] - {PR_MERGE_DATE}
---
+++ src/components/PasswordComp.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const PasswordComp = (props: Props) => {
+  return <div>PasswordComp</div>;
+};
+
+export default PasswordComp;
